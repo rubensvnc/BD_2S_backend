7ecf067cb81072a30266688b93d54e80a01bcda7
Breno Andrade <148828340+brenobsa@users.noreply.github.com>
2026-04-30 18:56:07 -0300

    Format Main2.java and remove unnecessary comments

--- src/main/java/org/example/demo3/Main2.java
+++ src/main/java/org/example/demo3/Main2.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main2 extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // O caminho deve ser relativo à pasta resources [cite: 70, 71]
+        
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/dashboard_adm.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SwiftPlan - Dashboard ADM");
