6fc06760c71117fda39d9641c07eae88cbe11a2e
Breno Andrade <148828340+brenobsa@users.noreply.github.com>
2026-04-30 18:57:14 -0300

    Update Datas_Restritas.java

--- src/main/java/org/example/demo3/entity/Datas_Restritas.java
+++ src/main/java/org/example/demo3/entity/Datas_Restritas.java
@@ -3,12 +3,12 @@ package org.example.demo3.entity;
 import java.time.LocalDate;
 
 public class Datas_Restritas {
-    private Integer id_restricao; // INT no banco
-    private LocalDate data_bloqueio; // DATE no banco
-    private String descricao; // VARCHAR(100) no banco
-    private Integer id_referencia; // INT no banco (FK)
+    private Integer id_restricao; 
+    private LocalDate data_bloqueio; 
+    private String descricao; 
+    private Integer id_referencia; 
 
-    // Construtor completo
+    
     public Datas_Restritas(Integer id_restricao, LocalDate data_bloqueio, String descricao, Integer id_referencia) {
         this.id_restricao = id_restricao;
         this.data_bloqueio = data_bloqueio;
@@ -16,7 +16,7 @@ public class Datas_Restritas {
         this.id_referencia = id_referencia;
     }
 
-    // Getters
+    
     public Integer getId_restricao() { return id_restricao; }
     public LocalDate getData_bloqueio() { return data_bloqueio; }
     public String getDescricao() { return descricao; }
