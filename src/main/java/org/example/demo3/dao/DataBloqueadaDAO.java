5a16b42d972e76899de52fb57ff39509d1f7e8c6
Wander717 <wandercpv07@gmail.com>
2026-05-13 21:51:51 -0300

    feat(Entidades/DAOS): Adicionadas todas as Entidades e Daos que serão utilizados.

---
+++ src/main/java/org/example/demo3/dao/DataBloqueadaDAO.java
@@ -0,0 +1,33 @@
+package org.example.demo3.entity;
+import java.time.LocalDate;
+
+public class DataCritica {
+    private LocalDate data_bloqueio;
+    private String descricao;
+    private Long id_restricao;
+    private Long id_referencia;
+
+
+    public DataCritica(LocalDate data_bloqueio, String descricao, Long id_restricao, Long id_referencia) {
+        this.data_bloqueio = data_bloqueio;
+        this.descricao = descricao;
+        this.id_restricao = id_restricao;
+        this.id_referencia = id_referencia;
+    }
+
+    public LocalDate getData_bloqueio() {
+        return data_bloqueio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Long getId_restricao() {
+        return id_restricao;
+    }
+
+    public Long getId_referencia() {
+        return id_referencia;
+    }
+}
