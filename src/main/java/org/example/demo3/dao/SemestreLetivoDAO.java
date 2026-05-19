d0f10f0ea7e756c7e907161c1fb77f0315ff02bc
rubensvnc <rubensvnc@gmail.com>
2026-05-19 17:17:39 -0300

    feat: adicionar busca de ano e semestre de ano baseado no id de um professor

---
+++ src/main/java/org/example/demo3/dao/SemestreLetivoDAO.java
@@ -0,0 +1,47 @@
+package org.example.demo3.dao;
+
+import org.example.demo3.DatabaseConnection;
+import org.example.demo3.entity.SemestreLetivo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SemestreLetivoDAO{
+    public List<SemestreLetivo> listarAnoESemestreAno(int professorId) throws SQLException {
+        String sql = """
+            SELECT DISTINCT sl.ano, sl.numero_semestre FROM atribuicao_professor
+            AS ap INNER JOIN semestre_letivo AS sl ON sl.id_semestre_letivo = semestre_letivo_id 
+            WHERE ap.professor_id = ?;
+            """;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        List<SemestreLetivo> lista = new ArrayList<>();
+        try {
+            conn = DatabaseConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, professorId);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                SemestreLetivo sl = new SemestreLetivo();
+                sl.setAno(rs.getInt("sl.ano"));
+                sl.setNumero_semestre(rs.getInt("sl.numero_semestre"));
+
+                lista.add(sl);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar temas: " + e.getMessage());
+            throw e;
+        } finally {
+            DatabaseConnection.closeConnection();
+        }
+
+        return lista;
+    }
+}
