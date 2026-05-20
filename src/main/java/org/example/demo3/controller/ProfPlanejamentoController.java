270f43de65a694c02d14ec38d5bae4ae0472fa6a
rubensvnc <rubensvnc@gmail.com>
2026-05-20 16:03:25 -0300

    feat: preenchimento dos metodos FXML

--- src/main/java/org/example/demo3/controller/ProfPlanejamentoController.java
+++ src/main/java/org/example/demo3/controller/ProfPlanejamentoController.java
@@ -1,4 +1,16 @@
 package org.example.demo3.controller;
 
+import javafx.fxml.FXML;
+
 public class ProfPlanejamentoController {
+
+
+    @FXML
+    public void handleGerarPlanejamento(){}
+
+    @FXML
+    public void handleMarcarMinistrada(){}
+
+    @FXML
+    public void handleCancelarSelecionados(){}
 }
