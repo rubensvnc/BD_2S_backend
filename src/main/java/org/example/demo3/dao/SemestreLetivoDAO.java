51aded2b8dc67b8be6ced8735a701eb781b241a6
rubensvnc <rubensvnc@gmail.com>
2026-05-20 17:10:02 -0300

    feat: adicionar busca de semestre e semestreAno para coordenadores

--- src/main/java/org/example/demo3/dao/SemestreLetivoDAO.java
+++ src/main/java/org/example/demo3/dao/SemestreLetivoDAO.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SemestreLetivoDAO{
-    public List<SemestreLetivo> listarAnoESemestreAno(int professorId) throws SQLException {
+    public List<SemestreLetivo> listarProfessorAnoESemestreAno(int professorId) throws SQLException {
         String sql = """
             SELECT DISTINCT sl.ano, sl.numero_semestre FROM atribuicao_professor
             AS ap INNER JOIN semestre_letivo AS sl ON sl.id_semestre_letivo = semestre_letivo_id 
@@ -26,6 +26,40 @@ public class SemestreLetivoDAO{
             conn = DatabaseConnection.getConnection();
             ps = conn.prepareStatement(sql);
             ps.setInt(1, professorId);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                SemestreLetivo sl = new SemestreLetivo();
+                sl.setAno(rs.getInt("sl.ano"));
+                sl.setNumero_semestre(rs.getInt("sl.numero_semestre"));
+
+                lista.add(sl);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar temas: " + e.getMessage());
+            throw e;
+        } finally {
+            DatabaseConnection.closeConnection();
+        }
+
+        return lista;
+    }
+
+    public List<SemestreLetivo> listarCoordenadorAnoESemestreAno(int coordId) throws SQLException {
+        String sql = """
+            SELECT DISTINCT sl.ano, sl.numero_semestre FROM horario_curso AS hc INNER JOIN 
+            semestre_letivo AS sl ON hc.semestre_letivo_id = sl.id_semestre_letivo 
+            INNER JOIN curso AS c ON c.id_curso = hc.curso_id WHERE c.coordenador_id = ?;
+            """;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        List<SemestreLetivo> lista = new ArrayList<>();
+        try {
+            conn = DatabaseConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, coordId);
 
             rs = ps.executeQuery();
             while (rs.next()) {
