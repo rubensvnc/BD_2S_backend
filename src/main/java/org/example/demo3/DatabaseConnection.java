a2db4bdae7c64f9614a7fa4c8d75b91e1b196bbd
rubensvnc <rubensvnc@gmail.com>
2026-05-20 21:52:15 -0300

    fix: remover falhas de seguranca que mostravam dados de login do banco

--- src/main/java/org/example/demo3/DatabaseConnection.java
+++ src/main/java/org/example/demo3/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/swiftplan";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String URL = ConfigDB.getUrl()+ConfigDB.getName();
+    private static final String USER = ConfigDB.getUsername();
+    private static final String PASSWORD = ConfigDB.getPassword();
 
     private static Connection connection = null;
 
@@ -17,7 +17,15 @@ public class DatabaseConnection {
             if (connection == null || connection.isClosed()) {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                // Monta os dados dinamicamente no momento da conexão
+                String url = ConfigDB.getUrl() + ConfigDB.getName();
+                String user = ConfigDB.getUsername();
+                String password = ConfigDB.getPassword();
+
+                // Log preventivo para você verificar se os dados deixaram de ser null
+                System.out.println("Tentando conectar em: " + url + " com o usuário: " + user);
+
+                connection = DriverManager.getConnection(url, user, password);
                 System.out.println("Conexão estabelecida com sucesso!");
             }
         } catch (ClassNotFoundException e) {
