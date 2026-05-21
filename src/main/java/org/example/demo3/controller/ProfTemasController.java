c000a0cf0779b15a808233b156bfe10c743ff42f
Wander717 <wandercpv07@gmail.com>
2026-05-20 22:35:36 -0300

    feat(ProfTemasController): Métodos de adicionar temas e listar adicionados.

--- src/main/java/org/example/demo3/controller/ProfTemasController.java
+++ src/main/java/org/example/demo3/controller/ProfTemasController.java
@@ -1,7 +1,9 @@
 package org.example.demo3.controller;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
 import org.example.demo3.dao.DependenciaTemaDAO;
 import org.example.demo3.dao.TemaDAO;
@@ -9,19 +11,20 @@ import org.example.demo3.entity.Disciplina;
 import org.example.demo3.entity.SemestreLetivo;
 import org.example.demo3.entity.Tema;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProfTemasController {
 
     //TABELA DE TEMAS
-    @FXML private TableView<?> tabelaTemas;
-    @FXML private TableColumn<?, ?> colTemaPrior;
-    @FXML private TableColumn<?, ?> colTemaNome;
-    @FXML private TableColumn<?, ?> colTemaMin;
-    @FXML private TableColumn<?, ?> colTemaMax;
-    @FXML private TableColumn<?, ?> colTemaAval;
-    @FXML private TableColumn<?, ?> colTemaOpc;
-    @FXML private TableColumn<?, ?> colTemaAcoes;
+    @FXML private TableView<Tema> tabelaTemas;
+    @FXML private TableColumn<Tema, Integer> colTemaPrior;
+    @FXML private TableColumn<Tema, String> colTemaNome;
+    @FXML private TableColumn<Tema, Integer> colTemaMin;
+    @FXML private TableColumn<Tema, Integer> colTemaMax;
+    @FXML private TableColumn<Tema, String> colTemaAval;
+    @FXML private TableColumn<Tema, String> colTemaOpc;
+    @FXML private TableColumn<Tema, String> colTemaAcoes;
     //FORMULÁRIO
     @FXML private Label lblTituloFormTema;
     @FXML private TextField tfTemaNome;
@@ -35,21 +38,20 @@ public class ProfTemasController {
     // DEPENDÊNCIAS
     @FXML private Label lblTemaSelecionadoDep;
     @FXML private Label lblErrCircular;
-    @FXML private ListView<?> listTemasDisponiveis;
-    @FXML private ListView<?> listDependencias;
+    @FXML private ListView<Tema> listTemasDisponiveis;
+    @FXML private ListView<Tema> listDependencias;
     //DAOS
     private TemaDAO temaDAO = new TemaDAO();
     private DependenciaTemaDAO dependenciaTemaDAO = new DependenciaTemaDAO();
-
     // INITIALIZE
     @FXML
     public void initialize() {
-
+        configurarSpinners();
+        listarTemasnaTabela();
+        carregarTemas();
     }
 
     // AÇÕES DE TEMAS
-
-
     @FXML
     private void handleImportarTemas() {
 
@@ -67,16 +69,55 @@ public class ProfTemasController {
 
     @FXML
     private void handleLimparTema() {
-
+        limparFormulario();
     }
 
     @FXML
     private void handleSalvarTema() {
+        try {
 
+            errTemaNome.setText("");
+
+            if (tfTemaNome.getText().isBlank()) {
+                errTemaNome.setText("Digite o nome do tema.");
+                return;
+            }
+            Tema tema = new Tema();
+
+            // IDs FIXOS PARA TESTE
+            tema.setDisciplina_id(1);
+            tema.setSemestre_letivo_id(3);
+
+            // CAMPOS DO FORMULÁRIO
+            tema.setNome(tfTemaNome.getText());
+            tema.setQtd_min_aulas(spTemaMin.getValue());
+            tema.setQtd_max_aulas(spTemaMax.getValue());
+            tema.setPrioridade(spTemaPrioridade.getValue());
+
+            // CHECKBOX → INT
+            tema.setEh_avaliacao(cbTemaAvaliacao.isSelected() ? 1 : 0);
+            tema.setEh_opcional(cbTemaOpcional.isSelected() ? 1 : 0);
+
+            // INSERT NO BANCO
+            temaDAO.inserirTema(tema);
+
+            // FEEDBACK
+            lblFeedbackTema.setText("Tema salvo com sucesso!");
+
+            // LIMPAR FORM
+            handleLimparTema();
+            carregarTemas();
+
+        } catch (Exception e) {
+
+            lblFeedbackTema.setText("Erro ao salvar tema.");
+
+            System.out.println(e.getMessage());
+        }
     }
 
-    // AÇÕES DE DEPENDÊNCIAS
 
+    // AÇÕES DE DEPENDÊNCIAS
     @FXML
     private void handleAdicionarDep() {
 
@@ -102,6 +143,67 @@ public class ProfTemasController {
 
     }
 
+    @FXML
+    private void limparFormulario() {
+        tfTemaNome.clear();
+        spTemaMin.getValueFactory().setValue(1);
+        spTemaMax.getValueFactory().setValue(1);
+        spTemaPrioridade.getValueFactory().setValue(1);
+        cbTemaAvaliacao.setSelected(false);
+        cbTemaOpcional.setSelected(false);
+    }
 
 
-}
+    private void carregarTemas() {
+
+        List<Tema> temas = temaDAO.listarTemas();
+
+        tabelaTemas.getItems().clear();
+
+        tabelaTemas.getItems().addAll(temas);
+    }
+
+    private void configurarSpinners() {
+        spTemaMin.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1)
+        );
+
+        spTemaMax.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1)
+        );
+
+        spTemaPrioridade.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 999, 1)
+        );
+    }
+
+    private void listarTemasnaTabela() {
+        colTemaPrior.setCellValueFactory(
+                new PropertyValueFactory<>("prioridade")
+        );
+
+        colTemaNome.setCellValueFactory(
+                new PropertyValueFactory<>("nome")
+        );
+
+        colTemaMin.setCellValueFactory(
+                new PropertyValueFactory<>("qtd_min_aulas")
+        );
+
+        colTemaMax.setCellValueFactory(
+                new PropertyValueFactory<>("qtd_max_aulas")
+        );
+
+        colTemaAval.setCellValueFactory(cellData ->
+                new SimpleStringProperty(
+                        cellData.getValue().getEh_avaliacao() == 1 ? "Sim" : "Não"
+                )
+        );
+
+        colTemaOpc.setCellValueFactory(cellData ->
+                new SimpleStringProperty(
+                        cellData.getValue().getEh_opcional() == 1 ? "Sim" : "Não"
+                )
+        );
+
+    }
