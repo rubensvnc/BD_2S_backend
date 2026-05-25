38d8d4ec1514f152420b6bcfeed6fa8ba2813439
rubensvnc <rubensvnc@gmail.com>
2026-05-24 21:14:28 -0300

    feat: adicionar metodo para listar os horarios de um template, baseado no turno

--- src/main/java/org/example/demo3/dao/TemplateHorarioTurnoDAO.java
+++ src/main/java/org/example/demo3/dao/TemplateHorarioTurnoDAO.java
@@ -1,6 +1,7 @@
 package org.example.demo3.dao;
 
 import java.sql.*;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +28,41 @@ public class TemplateHorarioTurnoDAO {
 
         stmt.close();
         conn.close();
+    }
+
+    public List<TemplateHorarioTurno> listarPorTurno(String turno) throws SQLException {
+        String sql = """
+            SELECT * FROM template_horario_turno WHERE turno = ? ORDER BY numero_ordem ASC;
+            """;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        List<TemplateHorarioTurno> lista = new ArrayList<>();
+        try {
+            conn = DatabaseConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, turno);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                TemplateHorarioTurno tht = new TemplateHorarioTurno();
+                tht.setId_template(rs.getInt("id_template"));
+                tht.setTurno(rs.getString("turno"));
+                tht.setTipo(rs.getString("tipo"));
+                tht.setNumero_ordem(rs.getInt("numero_ordem"));
+                tht.setHora_inicio(rs.getObject("hora_inicio", LocalTime.class));
+                tht.setHora_fim(rs.getObject("hora_fim", LocalTime.class));
+
+                lista.add(tht);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            DatabaseConnection.closeConnection();
+        }
+
+        return lista;
     }
 
     public List<TemplateHorarioTurno> listar() throws SQLException {
