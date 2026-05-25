4f317e02c6318995cfa679158ccb1f054175be56
Wander717 <wandersonrsantos01@gmail.com>
2026-05-24 23:19:46 -0300

    feat(DAO): TemaDAO atualizado.

--- src/main/java/org/example/demo3/dao/TemaDAO.java
+++ src/main/java/org/example/demo3/dao/TemaDAO.java
@@ -73,62 +73,36 @@ public class TemaDAO {
         }
 
         //LISTA OS TEMAS POR DISCIPLINA E SEMESTRE
-    public List<Tema> listarTemasPorDisciplinaESemestre(
-            int disciplina_id,
-            int semestre_letivo_id
-    ) {
+        public List<Tema> listarTemasPorDisciplinaESemestre(int idDisciplina, int idSemestre) {
+            List<Tema> lista = new ArrayList<>();
+            String sql = "SELECT * FROM tema WHERE disciplina_id = ? AND semestre_letivo_id = ? AND deletado_em IS NULL ORDER BY prioridade ASC";
 
-        String sql = """
-    SELECT * FROM tema
-    WHERE disciplina_id = ?
-      AND semestre_letivo_id = ?
-      AND deletado_em IS NULL
-    ORDER BY prioridade ASC
-    """;
+            try (Connection conn = DatabaseConnection.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        List<Tema> temas = new ArrayList<>();
+                stmt.setInt(1, idDisciplina);
+                stmt.setInt(2, idSemestre);
 
-        try (
-                Connection connection =
-                        DatabaseConnection.getConnection();
-
-                PreparedStatement stmt =
-                        connection.prepareStatement(sql)
-        ) {
-
-            stmt.setInt(1, disciplina_id);
-            stmt.setInt(2, semestre_letivo_id);
-
-            try (ResultSet rs = stmt.executeQuery()) {
-
-                while (rs.next()) {
-
-                    Tema tema = new Tema();
-
-                    tema.setId_tema(rs.getInt("id_tema"));
-                    tema.setDisciplina_id(rs.getInt("disciplina_id"));
-                    tema.setSemestre_letivo_id(rs.getInt("semestre_letivo_id"));
-                    tema.setNome(rs.getString("nome"));
-                    tema.setEh_avaliacao(rs.getInt("eh_avaliacao"));
-                    tema.setQtd_min_aulas(rs.getInt("qtd_min_aulas"));
-                    tema.setQtd_max_aulas(rs.getInt("qtd_max_aulas"));
-                    tema.setPrioridade(rs.getInt("prioridade"));
-                    tema.setEh_opcional(rs.getInt("eh_opcional"));
-
-                    temas.add(tema);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while (rs.next()) {
+                        Tema t = new Tema();
+                        t.setId_tema(rs.getInt("id_tema"));
+                        t.setNome(rs.getString("nome"));
+                        t.setQtd_min_aulas(rs.getInt("qtd_min_aulas"));
+                        t.setQtd_max_aulas(rs.getInt("qtd_max_aulas"));
+                        t.setPrioridade(rs.getInt("prioridade"));
+                        t.setEh_avaliacao(rs.getInt("eh_avaliacao"));
+                        t.setEh_opcional(rs.getInt("eh_opcional"));
+                        t.setDisciplina_id(rs.getInt("disciplina_id"));
+                        t.setSemestre_letivo_id(rs.getInt("semestre_letivo_id"));
+                        lista.add(t);
+                    }
                 }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-        } catch (SQLException e) {
-
-            System.out.println(
-                    "Erro ao listar temas: "
-                            + e.getMessage()
-            );
+            return lista;
         }
-
-        return temas;
-    }
     //EDITA TEMA
     public void editarTema(Tema tema) {
 
