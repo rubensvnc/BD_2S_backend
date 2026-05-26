4196a506711f67513518dfd00ce37b90c59a040d
Froguie <matheushan06@gmail.com>
2026-05-25 21:17:21 -0300

    feat: adicionar dois metodos vazios e um import para nao dar conflito, adicionando os metodos sem definicao nos botoes do login

--- src/main/java/org/example/demo3/controller/LoginController.java
+++ src/main/java/org/example/demo3/controller/LoginController.java
@@ -1,4 +1,10 @@
 package org.example.demo3.controller;
 
+import javafx.fxml.FXML;
+
 public class LoginController {
+    @FXML
+    void handleLogin() { }
+    @FXML
+    void handleCadastrarPrimeiroAdm() { }
 }
