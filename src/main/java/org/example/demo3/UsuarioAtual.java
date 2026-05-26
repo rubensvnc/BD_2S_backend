72b284bef5ffe0ad232556e65f7549a70d7e2a3d
rubensvnc <rubensvnc@gmail.com>
2026-05-26 11:40:10 -0300

    feat: substituir os atributos para escutarem mudanca de valores em tempo real, usando os tipos ObjectProperty

--- src/main/java/org/example/demo3/UsuarioAtual.java
+++ src/main/java/org/example/demo3/UsuarioAtual.java
@@ -1,16 +1,25 @@
 package org.example.demo3;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class UsuarioAtual {
     private static UsuarioAtual instancia;
-    private Integer id_usuario;
-    private String tipo;
 
-    //CONTEXTO GLOBAL
-    private Integer ano;
-    private Integer anoSemestre;
-    private String curso;
-    private Integer semestreCurso;
-    private String disciplina;
+    // Propriedades do Usuário
+    private final ObjectProperty<Integer> id_usuario = new SimpleObjectProperty<>(null);
+    private final StringProperty tipo = new SimpleStringProperty(null);
+
+    // Contexto Global
+    private final ObjectProperty<Integer> ano = new SimpleObjectProperty<>(null);
+    private final ObjectProperty<Integer> anoSemestre = new SimpleObjectProperty<>(null);
+    private final ObjectProperty<Integer> id_curso = new SimpleObjectProperty<>(null);
+    private final ObjectProperty<Integer> semestreCurso = new SimpleObjectProperty<>(null);
+    private final ObjectProperty<Integer> id_disciplina = new SimpleObjectProperty<>(null);
 
     private UsuarioAtual() {}
 
@@ -21,84 +30,55 @@ public class UsuarioAtual {
         return instancia;
     }
 
+    public void limparSessao() {
+        setId_usuario(null);
+        setTipo(null);
+        setAno(null);
+        setAnoSemestre(null);
+        setIdCurso(null);
+        setSemestreCurso(null);
+        setIdDisciplina(null);
+    }
+
     public void usuarioAdm(){
-        this.curso = null;
-        this.semestreCurso = null;
-        this.disciplina = null;
+        setIdCurso(null);
+        setSemestreCurso(null);
+        setIdDisciplina(null);
     }
 
     public void usuarioCoord(){
-        this.semestreCurso = null;
-        this.disciplina = null;
+        setSemestreCurso(null);
+        setIdDisciplina(null);
     }
 
-    public void limparSessao() {
-        this.id_usuario = null;
-        this.tipo = null;
-        this.ano = null;
-        this.anoSemestre = null;
-        this.curso = null;
-        this.semestreCurso = null;
-        this.disciplina = null;
-    }
+    public ObjectProperty<Integer> idDisciplinaProperty() { return id_disciplina; }
+    public ObjectProperty<Integer> anoSemestreProperty() { return anoSemestre; }
+    public ObjectProperty<Integer> idCursoProperty() { return id_curso; }
+    public ObjectProperty<Integer> semestreCursoProperty() { return semestreCurso; }
+    public ObjectProperty<Integer> anoProperty() { return ano; }
 
-    public static void setInstancia(UsuarioAtual instancia) {
-        UsuarioAtual.instancia = instancia;
-    }
+    public Integer getId_usuario() { return id_usuario.get(); }
+    public void setId_usuario(Integer id) { this.id_usuario.set(id); }
 
-    public Integer getId_usuario() {
-        return id_usuario;
-    }
+    public String getTipo() { return tipo.get(); }
+    public void setTipo(String tipo) { this.tipo.set(tipo); }
 
-    public void setId_usuario(Integer id_usuario) {
-        this.id_usuario = id_usuario;
-    }
+    public Integer getAno() { return ano.get(); }
+    public void setAno(Integer ano) { this.ano.set(ano); }
 
-    public String getTipo() {
-        return tipo;
-    }
+    public Integer getAnoSemestre() { return anoSemestre.get(); }
+    public void setAnoSemestre(Integer anoSemestre) { this.anoSemestre.set(anoSemestre); }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+    public Integer getIdCurso() { return id_curso.get(); }
+    public void setIdCurso(Integer idCurso) { this.id_curso.set(idCurso); }
 
-    public Integer getAno() {
-        return ano;
-    }
+    public Integer getSemestreCurso() { return semestreCurso.get(); }
+    public void setSemestreCurso(Integer semestreCurso) { this.semestreCurso.set(semestreCurso); }
 
-    public void setAno(Integer ano) {
-        this.ano = ano;
-    }
+    public Integer getIdDisciplina() { return id_disciplina.get(); }
 
-    public Integer getAnoSemestre() {
-        return anoSemestre;
-    }
-
-    public void setAnoSemestre(Integer anoSemestre) {
-        this.anoSemestre = anoSemestre;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-    public Integer getSemestreCurso() {
-        return semestreCurso;
-    }
-
-    public void setSemestreCurso(Integer semestreCurso) {
-        this.semestreCurso = semestreCurso;
-    }
-
-    public String getDisciplina() {
-        return disciplina;
-    }
-
-    public void setDisciplina(String disciplina) {
-        this.disciplina = disciplina;
+    // CORRIGIDO: Agora aponta para id_disciplina e não para id_usuario!
+    public void setIdDisciplina(Integer idDisciplina) {
+        this.id_disciplina.set(idDisciplina);
     }
 }
