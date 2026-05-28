81009d72d5224735517ca9c1d5ceae55c09d1e0d
rubensvnc <rubensvnc@gmail.com>
2026-05-27 21:46:13 -0300

    feat: adicionar metodo para salvar uma lista de template de um turno em TemplateHorarioTurnoDAO
    feat: adicionar metodo para remover um template de um turno do banco

--- src/main/java/org/example/demo3/dao/TemplateHorarioTurnoDAO.java
+++ src/main/java/org/example/demo3/dao/TemplateHorarioTurnoDAO.java
@@ -10,6 +10,45 @@ import org.example.demo3.entity.*;
 
 public class TemplateHorarioTurnoDAO {
 
+    public void deletarTemplateTurno(String turno) {
+        String sql = """
+                DELETE FROM template_horario_turno WHERE turno = ?
+            """;
+
+        try (Connection conexao = DatabaseConnection.getConnection();
+             PreparedStatement ps = conexao.prepareStatement(sql)) {
+
+            ps.setString(1, turno);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao deletar dados no banco: " + e.getMessage());
+        }
+    }
+
+    public void salvarListaTemplate(List<TemplateHorarioTurno> listaTHT){
+        String sql = """
+                INSERT INTO template_horario_turno 
+                    (turno, tipo, numero_ordem, hora_inicio, hora_fim) VALUES (?, ?, ?, ?, ?)
+                """;
+
+        try (Connection conexao = DatabaseConnection.getConnection();
+             PreparedStatement ps = conexao.prepareStatement(sql)) {
+
+            for (TemplateHorarioTurno tht: listaTHT){
+                ps.setString(1, tht.getTurno());
+                ps.setString(2, tht.getTipo());
+                ps.setInt(3, tht.getNumero_ordem());
+                ps.setObject(4, tht.getHora_inicio());
+                ps.setObject(5, tht.getHora_fim());
+                ps.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao inserir dados no banco: " + e.getMessage());
+        }
+    }
+
     public void salvar(TemplateHorarioTurno objeto) throws SQLException {
         String sql = "INSERT INTO template_horario_turno (turno, tipo, numero_ordem, hora_inicio, hora_fim) VALUES (?, ?, ?, ?, ?)";
 
