a22c088c84b25e97b31085167790c908a6721f3f
rubensvnc <rubensvnc@gmail.com>
2026-05-31 18:19:37 -0300

    feat: adicionar duas formas de usar o db.properties, por um arquivo external (instalacao), ou dentro do propria IDE (desenvolvedor)

--- src/main/java/org/example/demo3/ConfigDB.java
+++ src/main/java/org/example/demo3/ConfigDB.java
@@ -1,5 +1,7 @@
 package org.example.demo3;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,15 +11,27 @@ public class ConfigDB {
     private static final Properties properties = new Properties();
 
     static {
-        // Carrega o arquivo dentro da pasta 'resources'
-        try (InputStream input = ConfigDB.class.getClassLoader().getResourceAsStream("db.properties")) {
-            if (input == null) {
-                System.err.println("Desculpe, não foi possível encontrar o arquivo db.properties");
-            } else {
+        // 1º tenta ler o db.properties externo (ao lado do .jar)
+        File externalConfig = new File("db.properties");
+        if (externalConfig.exists()) {
+            try (InputStream input = new FileInputStream(externalConfig)) {
                 properties.load(input);
+                System.out.println("Configuração carregada do arquivo externo.");
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } else {
+            // 2º fallback: lê o interno do resources (ambiente de desenvolvimento)
+            try (InputStream input = ConfigDB.class.getClassLoader().getResourceAsStream("db.properties")) {
+                if (input == null) {
+                    System.err.println("Arquivo db.properties não encontrado.");
+                } else {
+                    properties.load(input);
+                    System.out.println("Configuração carregada do resources interno.");
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
